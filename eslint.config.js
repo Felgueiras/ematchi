cf81c6cde98d3634f28a9292d2345c03cc5108c2
Felgueiras <7281851+Felgueiras@users.noreply.github.com>
2025-05-04 10:21:44 +0100

    configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+	{
+		files: ['**/*.ts', '**/*.js'],
+		ignores: ['node_modules/', 'build/', 'dist/', 'static/', '.svelte-kit/', '*.css'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module'
+			}
+		},
+		plugins: {
+			'@typescript-eslint': tseslint
+		},
+		rules: {
+			...tseslint.configs.recommended.rules
+		}
+	}
+];
