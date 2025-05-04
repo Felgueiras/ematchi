71621e9354ab06f8d4e0778bfcf3d18295508aca
Felgueiras <7281851+Felgueiras@users.noreply.github.com>
2025-05-04 10:29:03 +0100

    refactor: update ESLint configuration to use defineConfig for improved structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,21 @@
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
+import { defineConfig } from 'eslint/config';
 
-export default [
-	{
-		files: ['**/*.js', '**/*.ts'],
-		ignores: ['node_modules/', 'build/', 'dist/', 'static/', '.svelte-kit/**', '*.css'],
-		languageOptions: {
-			parser: tsParser,
-			parserOptions: {
-				ecmaVersion: 'latest',
-				sourceType: 'module'
-			}
-		},
-		plugins: {
-			'@typescript-eslint': tseslint
-		},
-		rules: {
-			...tseslint.configs.recommended.rules
+export default defineConfig({
+	files: ['**/*.js', '**/*.ts'],
+	ignores: ['node_modules/', 'build/', 'dist/', 'static/', '.svelte-kit/**', '*.css'],
+	languageOptions: {
+		parser: tsParser,
+		parserOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module'
 		}
+	},
+	plugins: {
+		'@typescript-eslint': tseslint
+	},
+	rules: {
+		...tseslint.configs.recommended.rules
 	}
-];
+});
